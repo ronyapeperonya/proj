a3699761f25fc3ec1b3eb0876f37b5fa3610a990
Ruslan Khasanov <rus_khas@mail.ru>
2024-09-03 01:52:59 +0500

    Доработка логики проекта

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,7 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("1111");
     }
 }
 
